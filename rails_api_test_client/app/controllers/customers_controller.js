e627928d33c1503f88b9a58d3bc5579d4e7f01ee
Tim <uniquelysimilar@yahoo.com>
2014-11-13 17:13:23 -0700

    continued CRUD implementation

--- rails_api_test_client/app/controllers/customers_controller.js
+++ rails_api_test_client/app/controllers/customers_controller.js
@@ -4,13 +4,72 @@ customersApp.controller('CustomerIndexController', function($scope, Customer) {
 .controller('CustomerShowController', function($scope, $routeParams, Customer) {
 	$scope.customer = Customer.get({id: $routeParams.id});
 })
-.controller('CustomerNewController', function($scope, Customer) {
+.controller('CustomerNewController', function($scope, Customer, $location) {
 	$scope.customer = new Customer();
-})
-.controller('CustomerCreateController', function($scope, Customer) {
-	console.log("Hello from CustomerCreateController");
+	// Set default state value
+	$scope.customer.state = 'Colorado';
 
-	$scope.message = "Hello from 'CustomerCreateController'";
+	$scope.states = {
+		'Alabama': 'Alabama',
+		'Alaska': 'Alaska',
+		'Arizona': 'Arizona',
+		'Arkansas': 'Arkansas',
+		'California': 'California',
+		'Colorado': 'Colorado',
+		'Connecticut': 'Connecticut',
+		'Delaware': 'Delaware',
+		'District of Columbia': 'District of Columbia',
+		'Florida': 'Florida',
+		'Georgia': 'Georgia',
+		'Hawaii': 'Hawaii',
+		'Idaho': 'Idaho',
+		'Illinois': 'Illinois',
+		'Indiana': 'Indiana',
+		'Iowa': 'Iowa',
+		'Kansas': 'Kansas',
+		'Kentucky': 'Kentucky',
+		'Louisiana': 'Louisiana',
+		'Maine': 'Maine',
+		'Maryland': 'Maryland',
+		'Massachusetts': 'Massachusetts',
+		'Michigan': 'Michigan',
+		'Minnesota': 'Minnesota',
+		'Mississippi': 'Mississippi',
+		'Missouri': 'Missouri',
+		'Montana': 'Montana',
+		'Nebraska': 'Nebraska',
+		'Nevada': 'Nevada',
+		'New Hampshire': 'New Hampshire',
+		'New Jersey': 'New Jersey',
+		'New Mexico': 'New Mexico',
+		'New York': 'New York',
+		'North Carolina': 'North Carolina',
+		'North Dakota': 'North Dakota',
+		'Ohio': 'Ohio',
+		'Oklahoma': 'Oklahoma',
+		'Oregon': 'Oregon',
+		'Pennsylvania': 'Pennsylvania',
+		'Puerto Rico': 'Puerto Rico',
+		'Rhode Island': 'Rhode Island',
+		'South Carolina': 'South Carolina',
+		'South Dakota': 'South Dakota',
+		'Tennessee': 'Tennessee',
+		'Texas': 'Texas',
+		'Utah': 'Utah',
+		'Vermont': 'Vermont',
+		'Virginia': 'Virginia',
+		'Washington': 'Washington',
+		'West Virginia': 'West Virginia',
+		'Wisconsin': 'Wisconsin',
+		'Wyoming': 'Wyoming'
+	};
+
+	$scope.saveCustomer = function() {
+		Customer.save($scope.customer);
+		// TODO: Handle possible error
+		$location.path('/');
+	}
+
 })
 .controller('CustomerUpdateController', function($scope, Customer) {
 	console.log("Hello from CustomerUpdateController");
