997fe28204a3438b4bb04ec6c88f9d6342455c68
Tim <uniquelysimilar@yahoo.com>
2014-11-14 17:34:43 -0700

    continuing CRUD implementation

--- rails_api_test_client/app/controllers/customers_controller.js
+++ rails_api_test_client/app/controllers/customers_controller.js
@@ -4,70 +4,30 @@ customersApp.controller('CustomerIndexController', function($scope, Customer) {
 .controller('CustomerShowController', function($scope, $routeParams, Customer) {
 	$scope.customer = Customer.get({id: $routeParams.id});
 })
-.controller('CustomerNewController', function($scope, Customer, $location) {
+.controller('CustomerNewController', function($scope, Customer, $window) {
 	$scope.customer = new Customer();
 	// Set default state value
 	$scope.customer.state = 'Colorado';
+	$scope.customer.home_phone = '303-555-1212';
+	$scope.customer.work_phone = '303-555-1212';
 
-	$scope.states = {
-		'Alabama': 'Alabama',
-		'Alaska': 'Alaska',
-		'Arizona': 'Arizona',
-		'Arkansas': 'Arkansas',
-		'California': 'California',
-		'Colorado': 'Colorado',
-		'Connecticut': 'Connecticut',
-		'Delaware': 'Delaware',
-		'District of Columbia': 'District of Columbia',
-		'Florida': 'Florida',
-		'Georgia': 'Georgia',
-		'Hawaii': 'Hawaii',
-		'Idaho': 'Idaho',
-		'Illinois': 'Illinois',
-		'Indiana': 'Indiana',
-		'Iowa': 'Iowa',
-		'Kansas': 'Kansas',
-		'Kentucky': 'Kentucky',
-		'Louisiana': 'Louisiana',
-		'Maine': 'Maine',
-		'Maryland': 'Maryland',
-		'Massachusetts': 'Massachusetts',
-		'Michigan': 'Michigan',
-		'Minnesota': 'Minnesota',
-		'Mississippi': 'Mississippi',
-		'Missouri': 'Missouri',
-		'Montana': 'Montana',
-		'Nebraska': 'Nebraska',
-		'Nevada': 'Nevada',
-		'New Hampshire': 'New Hampshire',
-		'New Jersey': 'New Jersey',
-		'New Mexico': 'New Mexico',
-		'New York': 'New York',
-		'North Carolina': 'North Carolina',
-		'North Dakota': 'North Dakota',
-		'Ohio': 'Ohio',
-		'Oklahoma': 'Oklahoma',
-		'Oregon': 'Oregon',
-		'Pennsylvania': 'Pennsylvania',
-		'Puerto Rico': 'Puerto Rico',
-		'Rhode Island': 'Rhode Island',
-		'South Carolina': 'South Carolina',
-		'South Dakota': 'South Dakota',
-		'Tennessee': 'Tennessee',
-		'Texas': 'Texas',
-		'Utah': 'Utah',
-		'Vermont': 'Vermont',
-		'Virginia': 'Virginia',
-		'Washington': 'Washington',
-		'West Virginia': 'West Virginia',
-		'Wisconsin': 'Wisconsin',
-		'Wyoming': 'Wyoming'
-	};
+	$scope.states = statesAry;
 
 	$scope.saveCustomer = function() {
-		Customer.save($scope.customer);
+		Customer.save($scope.customer,
+			function(value, headers){
+				console.log(value);
+				$window.location.href = '/rails_api_test_client/index.html';
+			},
+			function(res){
+				// NOTE: This error callback not called with standard setup.  Found info on web.
+				console.log('Error callback called')
+
+			});
+//		Customer.save($scope.customer);
 		// TODO: Handle possible error
-		$location.path('/');
+		// Force a reload of the customer 'index' page
+//		$window.location.href = '/rails_api_test_client/index.html';
 	}
 
 })
@@ -81,3 +41,58 @@ customersApp.controller('CustomerIndexController', function($scope, Customer) {
 
 	$scope.message = "Hello from 'CustomerDestroyController'";
 });
+
+var statesAry = {
+	'Alabama': 'Alabama',
+	'Alaska': 'Alaska',
+	'Arizona': 'Arizona',
+	'Arkansas': 'Arkansas',
+	'California': 'California',
+	'Colorado': 'Colorado',
+	'Connecticut': 'Connecticut',
+	'Delaware': 'Delaware',
+	'District of Columbia': 'District of Columbia',
+	'Florida': 'Florida',
+	'Georgia': 'Georgia',
+	'Hawaii': 'Hawaii',
+	'Idaho': 'Idaho',
+	'Illinois': 'Illinois',
+	'Indiana': 'Indiana',
+	'Iowa': 'Iowa',
+	'Kansas': 'Kansas',
+	'Kentucky': 'Kentucky',
+	'Louisiana': 'Louisiana',
+	'Maine': 'Maine',
+	'Maryland': 'Maryland',
+	'Massachusetts': 'Massachusetts',
+	'Michigan': 'Michigan',
+	'Minnesota': 'Minnesota',
+	'Mississippi': 'Mississippi',
+	'Missouri': 'Missouri',
+	'Montana': 'Montana',
+	'Nebraska': 'Nebraska',
+	'Nevada': 'Nevada',
+	'New Hampshire': 'New Hampshire',
+	'New Jersey': 'New Jersey',
+	'New Mexico': 'New Mexico',
+	'New York': 'New York',
+	'North Carolina': 'North Carolina',
+	'North Dakota': 'North Dakota',
+	'Ohio': 'Ohio',
+	'Oklahoma': 'Oklahoma',
+	'Oregon': 'Oregon',
+	'Pennsylvania': 'Pennsylvania',
+	'Puerto Rico': 'Puerto Rico',
+	'Rhode Island': 'Rhode Island',
+	'South Carolina': 'South Carolina',
+	'South Dakota': 'South Dakota',
+	'Tennessee': 'Tennessee',
+	'Texas': 'Texas',
+	'Utah': 'Utah',
+	'Vermont': 'Vermont',
+	'Virginia': 'Virginia',
+	'Washington': 'Washington',
+	'West Virginia': 'West Virginia',
+	'Wisconsin': 'Wisconsin',
+	'Wyoming': 'Wyoming'
+};
