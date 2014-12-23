557ffffaf6be850eaff9e306a61fad237de6744a
Tim <uniquelysimilar@yahoo.com>
2014-12-22 17:10:39 -0700

    began contact api test client

---
+++ contact_api_test_client/angular_app/app.module.js
@@ -0,0 +1,21 @@
+var contactApp = angular.module("contactApp", []);
+
+contactApp.controller('ContactController', function($scope, $http) {
+	$scope.contact = {};
+	$scope.status = "";
+
+	$scope.processContact = function() {
+		// Simple POST request example (passing data) :
+		$http.post('http://associations.localhost/api/contacts', $scope.contact).
+			success(function(data, status, headers, config) {
+				// this callback will be called asynchronously
+				// when the response is available
+				$scope.status = status;
+			}).
+			error(function(data, status, headers, config) {
+				// called asynchronously if an error occurs
+				// or server returns response with an error status.
+				$scope.status = status;
+			});
+	};
+});
