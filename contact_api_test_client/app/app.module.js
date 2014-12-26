9966c0cf0ff486e4be5d7513f2e961a14de0f6e8
Tim <uniquelysimilar@yahoo.com>
2014-12-26 10:29:45 -0700

    added production url

--- contact_api_test_client/app/app.module.js
+++ contact_api_test_client/app/app.module.js
@@ -1,15 +1,16 @@
 var contactApp = angular.module("contactApp", []);
 
 contactApp.controller('ContactController', function($scope, $http) {
-//	$scope.contact = {};
 	$scope.status = "";
 	$scope.errors = "";
 	$scope.showDebug = false;
 	$scope.success = false;
+	contactServiceURL = "http://contact-service-timc.herokuapp.com/api/contacts"
+//	contactServiceURL = "http://contactservice.localhost/api/contacts"
 
 	$scope.processContact = function() {
 		// Simple POST request example (passing data) :
-		$http.post('http://contactservice.localhost/api/contacts', $scope.contact).
+		$http.post(contactServiceURL, $scope.contact).
 			success(function(data, status, headers, config) {
 				// this callback will be called asynchronously when the response is available
 				$scope.status = status;
