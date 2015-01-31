64f04a650afd9e19fa15c4d52321a01a95dd6152
Tim <uniquelysimilar@yahoo.com>
2015-01-31 10:38:42 -0700

    add/remove image impl

--- custom_directives/app/components/custom/images/imagesController.js
+++ custom_directives/app/components/custom/images/imagesController.js
@@ -1,9 +1,9 @@
 directiveApp.controller('imagesController', ['$scope', '$http', function($scope, $http) {
 	// Add/remove images processing
-	$scope.img_count = 1;
+	$scope.image_count = 1;
 	$scope.image_html = "<p>Uninitialized image HTML</p>";
-	$scope.images = [];
-
+	$scope.image_numbers = [];
+/*
 	$http.get('app/components/custom/images/image_container.html').
 	success(function(data, status) {
 		$scope.image_html = data;
@@ -12,13 +12,24 @@ directiveApp.controller('imagesController', ['$scope', '$http', function($scope,
 	error(function(data, status) {
 		console.error("imagesController '$http.get()' failed.")
 	});
-
+*/
 	$scope.addImage = function() {
 		console.log("imagesController 'addImage()' called.");
-//		$scope.images.push($scope.image_html);
+		$scope.image_numbers.push($scope.image_count++);
+	};
+
+	$scope.removeImage = function() {
+		console.log("imagesController 'removeImage()' called.");
+		// Remove the associated object from the image array
 	};
 
 	$scope.resetPage = function() {
 		console.log("imagesController 'resetPage()' called.");
+		
+		while($scope.image_numbers.length > 0) {
+			$scope.image_numbers.pop();
+		}
+
+		$scope.image_count = 1;
 	};
 }]);
