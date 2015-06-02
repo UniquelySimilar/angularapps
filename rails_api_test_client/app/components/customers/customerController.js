fb9df3e9dce93bcc7d99a6533ab81a4f1d7d785b
Tim <uniquelysimilar@yahoo.com>
2015-06-02 16:15:58 -0600

    moved state select options to a service

--- rails_api_test_client/app/components/customers/customerController.js
+++ rails_api_test_client/app/components/customers/customerController.js
@@ -27,7 +27,7 @@ customersApp.controller('CustomerIndexController', function($scope, CustomerFact
 	$scope.customer = CustomerFactory.get({id: $routeParams.id});
 })
 
-.controller('CustomerNewController', function($scope, CustomerFactory, $location) {
+.controller('CustomerNewController', function($scope, $location, CustomerFactory, stateOptions) {
 	$scope.customer = new CustomerFactory();
 	// Set default state value
 	$scope.customer.state = 'Colorado';
@@ -53,7 +53,7 @@ customersApp.controller('CustomerIndexController', function($scope, CustomerFact
 	}
 })
 
-.controller('CustomerEditController', function($scope, $routeParams, $location, CustomerFactory) {
+.controller('CustomerEditController', function($scope, $routeParams, $location, CustomerFactory, stateOptions) {
 	$scope.customer = CustomerFactory.get({id: $routeParams.id});
 	$scope.stateOpts = stateOptions;
 
@@ -72,59 +72,3 @@ customersApp.controller('CustomerIndexController', function($scope, CustomerFact
 			});
 	}
 })
-
-// TODO: Put this object in a service
-var stateOptions = {
-	'Alabama': 'Alabama',
-	'Alaska': 'Alaska',
-	'Arizona': 'Arizona',
-	'Arkansas': 'Arkansas',
-	'California': 'California',
-	'Colorado': 'Colorado',
-	'Connecticut': 'Connecticut',
-	'Delaware': 'Delaware',
-	'District of Columbia': 'District of Columbia',
-	'Florida': 'Florida',
-	'Georgia': 'Georgia',
-	'Hawaii': 'Hawaii',
-	'Idaho': 'Idaho',
-	'Illinois': 'Illinois',
-	'Indiana': 'Indiana',
-	'Iowa': 'Iowa',
-	'Kansas': 'Kansas',
-	'Kentucky': 'Kentucky',
-	'Louisiana': 'Louisiana',
-	'Maine': 'Maine',
-	'Maryland': 'Maryland',
-	'Massachusetts': 'Massachusetts',
-	'Michigan': 'Michigan',
-	'Minnesota': 'Minnesota',
-	'Mississippi': 'Mississippi',
-	'Missouri': 'Missouri',
-	'Montana': 'Montana',
-	'Nebraska': 'Nebraska',
-	'Nevada': 'Nevada',
-	'New Hampshire': 'New Hampshire',
-	'New Jersey': 'New Jersey',
-	'New Mexico': 'New Mexico',
-	'New York': 'New York',
-	'North Carolina': 'North Carolina',
-	'North Dakota': 'North Dakota',
-	'Ohio': 'Ohio',
-	'Oklahoma': 'Oklahoma',
-	'Oregon': 'Oregon',
-	'Pennsylvania': 'Pennsylvania',
-	'Puerto Rico': 'Puerto Rico',
-	'Rhode Island': 'Rhode Island',
-	'South Carolina': 'South Carolina',
-	'South Dakota': 'South Dakota',
-	'Tennessee': 'Tennessee',
-	'Texas': 'Texas',
-	'Utah': 'Utah',
-	'Vermont': 'Vermont',
-	'Virginia': 'Virginia',
-	'Washington': 'Washington',
-	'West Virginia': 'West Virginia',
-	'Wisconsin': 'Wisconsin',
-	'Wyoming': 'Wyoming'
-};
