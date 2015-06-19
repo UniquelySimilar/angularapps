e0b742b1a79ca01846a0dbe00991f65ecdaaf119
Tim <uniquelysimilar@yahoo.com>
2015-06-19 14:42:54 -0600

    DRYd up the api client resource service a bit

--- rails_api_test_client/app/components/customers/customerFactory.js
+++ rails_api_test_client/app/components/customers/customerFactory.js
@@ -1,28 +1,23 @@
 customersApp.factory('CustomerFactory', function($resource) {
+	var authToken = 'b7627f6e9087c69f';
+	var authHeader = { 'Authorization': 'Token token=' + authToken };
+
 	return $resource('http://railsapitestserver.localhost/customerapi/customers/:id', {id: '@id'}, {
 		get: {
 			method: 'GET',
-			headers: {
-				'Authorization': 'Token token=b7627f6e9087c69f'
-			}
+			headers: authHeader
 		},
 		save: {
 			method: 'POST',
-			headers: {
-				'Authorization': 'Token token=b7627f6e9087c69f'
-			}
+			headers: authHeader
 		},
 		update: {
 			method: 'PUT',
-			headers: {
-				'Authorization': 'Token token=b7627f6e9087c69f'
-			}
+			headers: authHeader
 		},
 		delete: {
 			method: 'DELETE',
-			headers: {
-				'Authorization': 'Token token=b7627f6e9087c69f'
-			}
+			headers: authHeader
 		}
 	});
 });
